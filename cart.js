e4a66e7b9275786e0596086741ca0813bc1b58cd
Sean Stevens <sstevens88@gmail.com>
2017-08-16 09:03:13 -0700

    initial commit

---
+++ cart.js
@@ -0,0 +1,37 @@
+const cartContents = {};
+let $cartItem = $('<tr></tr>');
+let $itemName = $('<td></td>', {
+    'class': 'item-name'
+});
+let $itemQuantity = $('<td></td>', {
+    'class': 'item-quantity'
+});
+let $itemPrice = $('<td></td>', {
+    'class': 'item-price'
+});
+
+$('.add-btn').click(function() {
+    const itemDetails = {};
+    let productName = $(this).siblings('.product-name').text();
+    if (cartContents[productName]) {
+        cartContents[productName].quantity += 1;
+    } else {
+        itemDetails.price = $(this).siblings('.product-price').text();
+        itemDetails.quantity = 1;
+        cartContents[productName] = itemDetails;
+    }
+    console.log(cartContents);
+    $('.cart').empty().append($('<table></table>', {
+        'class': 'table'
+    }));
+    $('.table').append($('<tbody></tbody>', {
+        'class': 'items-list'
+    }));
+    $.each(cartContents, function(name, details) {
+        let item = $cartItem.clone();
+        item.append($itemQuantity.clone().text(details.quantity));
+        item.append($itemName.clone().text(name));
+        item.append($itemPrice.clone().text(details.price));
+        item.appendTo($('.items-list'));
+    });
+});
