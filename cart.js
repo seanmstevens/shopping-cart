c209a5a9ca3da8cff2880f096d7c3cce6555d4fa
Sean Stevens <sstevens88@gmail.com>
2017-08-16 17:18:22 -0700

    Implemented Bootstrap 4

--- cart.js
+++ cart.js
@@ -15,8 +15,9 @@ $('.add-btn').click(function() {
     let productName = $(this).siblings('.product-name').text();
     if (cartContents[productName]) {
         cartContents[productName].quantity += 1;
+        cartContents[productName].price += Number($(this).siblings('.product-price').text().match(/[^$](.*)/)[0]);
     } else {
-        itemDetails.price = $(this).siblings('.product-price').text();
+        itemDetails.price = Number($(this).siblings('.product-price').text().match(/[^$](.*)/)[0]);
         itemDetails.quantity = 1;
         cartContents[productName] = itemDetails;
     }
@@ -29,9 +30,9 @@ $('.add-btn').click(function() {
     }));
     $.each(cartContents, function(name, details) {
         let item = $cartItem.clone();
-        item.append($itemQuantity.clone().text(details.quantity));
+        item.append($itemQuantity.clone().text(details.quantity).addClass('bold'));
         item.append($itemName.clone().text(name));
-        item.append($itemPrice.clone().text(details.price));
+        item.append($itemPrice.clone().text('$' + details.price));
         item.appendTo($('.items-list'));
     });
 });
